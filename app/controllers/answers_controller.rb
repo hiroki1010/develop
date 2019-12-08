dc719b8cb6017d929f7eb451d2dc7e3b040cd5e3
hiroki <pengin12345@docomo.ne.jp>
2019-12-08 16:27:08 +0900

    fifth

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,12 +6,33 @@ class AnswersController < ApplicationController
     if @answer.update(answer_params)
       redirect_to question_path(@question), notice: 'Success!'
     else
-      redirect_to question_path(@question), alert: 'Invalid!'
+     p @answer.errors
+      redirect_to question_path(@question), alert: 'Invalid'
     end
 
   end
 
   def edit
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    if @answer.update(answer_params)
+      redirect_to question_path(@question), notice: 'Success!'
+    else
+      flash[:alert] = 'Invalid!'
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    @answer.destroy
+    redirect_to question_path(@question),notice: 'Deleted!'
   end
 
   private
